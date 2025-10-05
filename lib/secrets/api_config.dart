1dd78f51af73c44513abdd1c783db6192bb2ee34
Do Hoang Huong Lien <luna@Dos-MacBook-Air.local>
2025-10-05 11:34:23 +0900

    Fetch map points

--- lib/secrets/api_config.dart
+++ lib/secrets/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
   // Base URL of your API
-  static const String baseUrl = "https://recycleledger";
+  static const String baseUrl = "https://ruco-test.recycleledger.net/api/";
 
   // API token (replace with your actual token)
   static const String apiToken = "YOUR_API_TOKEN_HERE";
--- lib/utils/map_points.dart
+++ lib/utils/map_points.dart
@@ -1,13 +1,32 @@
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:convert';
 
-class MapPoints {
-  // List of coordinates to show on the map
-  static final List<LatLng> points = [
-    LatLng(3.1490659,101.6910843),
-    LatLng(3.1653606,101.7110819),
-    LatLng(3.1554555,101.6940897),
-    LatLng(3.1437341,101.705376),
-  ];
+import 'package:google_map_test/data/mapper/map_points_mapper.dart';
+import 'package:google_map_test/secrets/api_config.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
+
+Future<List<LatLng>> fetchMapPoints() async {
+  final response = await http.get(
+    Uri.parse("${ApiConfig.baseUrl}/"),
+    headers: {
+      "Authorization": "Bearer ${ApiConfig.apiToken}",
+      "Content-Type": "application/json"
+    },
+  );
+
+  if (response.statusCode!=200){
+    throw Exception("Fail to fetch map points: ${response.statusCode}");
+  }
+
+  final List<dynamic> data = jsonDecode(response.body);
+
+  final List<LatLng> latLngPoints = data
+    .map((json)=> MapPointsMapper.fromJson(json))
+    .take(200)
+    .map((e) => LatLng(e.latitude, e.longitude))
+    .toList();
+
+  return latLngPoints;
 }
 
 
